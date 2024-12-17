476c8e7287ed324b4b07ca72e6afbb5279870d17
choco798 <choco798>
2024-12-17 20:30:38 +0900

    add_image

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120)
+        pyxel.load("main.pyxres")
         self.x = 0
         pyxel.run(self.update, self.draw)
 
@@ -12,5 +13,6 @@ class App:
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.blt(20, 20, 0, 0, 0, 16, 16, 0)
 
 App()
