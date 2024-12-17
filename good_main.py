9674cdb03831b509655ba6bab6aa52fdacd0619f
choco798 <choco798>
2024-12-18 06:45:24 +0900

    try_image

--- good_main.py
+++ good_main.py
@@ -3,16 +3,14 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120)
-        pyxel.load("main.pyxres")
         self.x = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 5) % pyxel.width
+        self.x = (self.x + 1) % pyxel.width
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
-        pyxel.blt(20, 20, 0, 0, 0, 16, 16, 0)
+        pyxel.rect(self.x, 8, 8, 8, 9)
 
 App()
